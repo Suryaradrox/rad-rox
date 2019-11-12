862f55c89102972c16e6d88dd3a75a7a62a9e9b7
Suryaradrox <57411508+Suryaradrox@users.noreply.github.com>
2019-11-12 20:03:50 +0530

    Add files via upload

---
+++ factorial through functions.c
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+int factorial (int x);
+int
+main ()
+{
+  int f, p;
+  f = factorial (p);
+  printf ("factorial of p is %d", f);
+  return 0;
+}
+
+int
+factorial (int x)
+{
+  int l, i, p;
+  printf ("give value of p =");
+  scanf("%d",&p);
+ 
+  i = l = 1;
+  while (i <= p)
+    {
+      l = l * i;
+      i = i + 1;
+    }
+  return l;
+}
